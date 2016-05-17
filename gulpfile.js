84c3d5103965e37387d8829a32bbdab533eb4169
smhasanmansur <smhasanmansur@gmail.com>
2016-05-17 11:51:26 +0600

    del in gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require("gulp");
 var del = require("del");
 
-gulp.task("testtask1", function () {
-    console.log("this is testtask1");
-});
-
 gulp.task("clean", function () {
     del.sync(["**/*.*~"]);
 });
 
-gulp.task("default", ["testtask1", "clean"], function () {
+gulp.task("default", ["clean"], function () {
     console.log("hello this is default gulp task"); 
+    gulp.watch("**/", ["clean"]);
+    console.log("bye this is default gulp task");
 });
